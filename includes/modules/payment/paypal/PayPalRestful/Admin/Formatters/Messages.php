f3c8fe8010623c0d93d8c177062afda49c1b0325
Chris Brown <code@drbyte.dev>
2025-09-16 18:17:07 -0400

    PHP 7.0 support: nullable types

--- includes/modules/payment/paypal/PayPalRestful/Admin/Formatters/Messages.php
+++ includes/modules/payment/paypal/PayPalRestful/Admin/Formatters/Messages.php
@@ -6,7 +6,7 @@
  * @copyright Copyright 2023 Zen Cart Development Team
  * @license https://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: lat9 2023 Nov 16 Modified in v2.0.0 $
- * 
+ *
  * Last updated: v1.2.0
  */
 namespace PayPalRestful\Admin\Formatters;
@@ -18,6 +18,8 @@ class Messages extends \messageStack
      * but this signature matches catalog-side class to allow
      * for simpler sharing of code that could run in either
      */
+    // NOTE: This '?string' nullable type requires PHP 7.1 or newer
+    // If you get compatibility errors, look at your catalog /includes/classes/messageStack.php class and make this function params the same as it.
     public function output(?string $class = null)
     {
         $this->table_data_parameters = 'class="pprNotification"';
